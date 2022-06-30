ce57f1b23aeadaeb61f6d01878d0167bcb782b9a
antoniowellington542 <antoniowellington542@gmail.com>
2022-06-30 13:53:30 -0300

    Rota post Users

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
+// Verifica se Já existe no Banco de dados
+function AlreadyExistUser(name){
+  
+  const exist = users.some((user) => user.name === name);
+
+  return exist;
+}
+
+// Rota para criar um novo usuario
 app.post('/users', (request, response) => {
   // Complete aqui
+  const { name, username} = request.body;
+
+  const exist = AlreadyExistUser(name);
+
+  if(!exist){
+    return response.status(400).json({ error: "User Already Exist!" });
+  }
+
+  const id = uuidv4();
+  
+  users.push({
+    id,
+    name,
+    username,
+    todos: []
+  });
+  
+  return response.status(201).send() 
 });
+
+
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
